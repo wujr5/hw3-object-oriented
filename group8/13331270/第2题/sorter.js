49d2b8ed6326357a34d369307ab822fd1b27d3d6
吴家荣 <973430584@qq.com>
2015-03-22 02:16:19 +0800

    13331270 update

--- group8/13331270/第2题/sorter.js
+++ group8/13331270/第2题/sorter.js
@@ -6,121 +6,150 @@ Tel:18819473271 Email:973430584@qq.com
 *******************************************/
 
 window.onload = function() {
-	var tables = getAllTables();
-	makeAllTablesSortable(tables);
+  var tables = getAllTables();
+  makeAllTablesSortable(tables);
 }
 //取得所有表格
 function getAllTables() {
-	var tables = document.getElementsByTagName("table");
-	return tables;
+  var tables = document.getElementsByTagName("table");
+  return tables;
 }
 //表格排序方法
 function sortTables(table, iCol) {
-    var ths = table.tHead.rows[0].cells;
-    var tbody = table.tBodies[0];
-    var colRows = tbody.rows;
-    var aTrs = new Array;
+  var ths = table.tHead.rows[0].cells;
+  var tbody = table.tBodies[0];
+  var colRows = tbody.rows;
+  var aTrs = new Array;
 
-    //将得到的列放入数组，备用
-    for (var i=0; i < colRows.length; i++) { 
-        aTrs[i] = colRows[i];  
-    }
+  //将得到的列放入数组，备用
+  for (var i=0; i < colRows.length; i++) { 
+    aTrs[i] = colRows[i];  
+  }
 
-    //改变成统一样式
-    for (var i = 0; i < ths.length; i++) {
-        ths[i].className = "nochange";
-    }
+  //改变成统一样式
+  for (var i = 0; i < ths.length; i++) {
+    ths[i].className = "nochange";
+  }
 
-    //判断上一次排列的列和现在需要排列的是否同一个。 
-    if (table.sortCol == iCol) {
-        aTrs.reverse();
-        
-        if (ths[iCol].change == "changeAscend") {
-            ths[iCol].className = "changeDescend";
-            ths[iCol].change = "changeDescend";
-        } else {
-            ths[iCol].className = "changeAscend";
-            ths[iCol].change = "changeAscend";
-        }
+  //判断上一次排列的列和现在需要排列的是否同一个。 
+  if (table.sortCol == iCol) {
+    aTrs.reverse();
+    
+    if (ths[iCol].change == "changeAscend") {
+      ths[iCol].className = "changeDescend";
+      ths[iCol].change = "changeDescend";
     } else {
-        aTrs.sort(compareEle(iCol));
-        ths[iCol].className = "changeAscend";
-        ths[iCol].change = "changeAscend";
+      ths[iCol].className = "changeAscend";
+      ths[iCol].change = "changeAscend";
     }
+  } else {
+    aTrs.sort(compareEle(iCol));
+    ths[iCol].className = "changeAscend";
+    ths[iCol].change = "changeAscend";
+  }
 
-    var oFragment = document.createDocumentFragment();  
+  var oFragment = document.createDocumentFragment();  
 
-    for (var i=0; i < aTrs.length; i++) {
-        oFragment.appendChild(aTrs[i]);
-    }
-    tbody.appendChild(oFragment);
+  for (var i=0; i < aTrs.length; i++) {
+    oFragment.appendChild(aTrs[i]);
+  }
+  tbody.appendChild(oFragment);
 
-    //记录最后一次排序的列索引 
-    table.sortCol = iCol;
+  //记录最后一次排序的列索引 
+  table.sortCol = iCol;
 }
 
 //排序函数，iCol表示列索引，dataType表示该列的数据类型  
 function compareEle(iCol) {
-    return  function (oTR1, oTR2) {
-        var vValue1 = oTR1.cells[iCol].textContent;
-        var vValue2 = oTR2.cells[iCol].textContent;
-        if (vValue1 < vValue2) {
-            return -1;
-        } else if (vValue1 > vValue2) {
-            return 1;
-        } else {
-            return 0;
-        }
-    };
+  return  function (oTR1, oTR2) {
+    var vValue1 = oTR1.cells[iCol].textContent;
+    var vValue2 = oTR2.cells[iCol].textContent;
+    if (vValue1 < vValue2) {
+      return -1;
+    } else if (vValue1 > vValue2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
 }
 
 //利用闭包，对每个表头添加鼠标点击事件
 function makeAllTablesSortable(tables) {
-    for (var i = 0; i < tables.length; i++) {
-        var ths = tables[i].tHead.rows[0].cells;
-        
-        for (var j = 0; j < ths.length; j++) {
-            ths[j].onclick = function() {
-                ths[j].index = j;
-                var table = tables[i];
-                return function() {
-                    sortTables(table, this.index);
-                };
-            }();
-        }
+  addSpanTagToEachLetter(tables);
+  for (var i = 0; i < tables.length; i++) {
+    var ths = tables[i].tHead.rows[0].cells;
+    
+    for (var j = 0; j < ths.length; j++) {
+      ths[j].onclick = function() {
+        ths[j].index = j;
+        var table = tables[i];
+        return function() {
+          sortTables(table, this.index);
+        };
+      }();
     }
+  }
+}
+
+function addSpanTagToEachLetter(tables) {
+  for (var i = 0; i < tables.length; i++) {
+    for (var j = 0; j < tables[i].tBodies[0].rows.length; j++) {
+      tables[i].tBodies[0].rows[j].cells = addSpan(tables[i].tBodies[0].rows[j].cells)
+    }
+  }
+}
+
+function addSpan(cells) {
+  for (var i = 0; i < cells.length; i++) {
+    var innerHTML = "";
+    for (var j = 0; j < cells[i].textContent.length; j++) {
+      innerHTML += '<span>' + cells[i].textContent[j] + '</span>';
+    }
+    cells[i].innerHTML = innerHTML;
+  }
+  return cells;
 }
 
 //下面是增加的表格过滤的函数，通过在html里面的onkeydown来实现实时过滤
 
 //对todo表格添加过滤函数
 function appendOnkeydownForTodo() {
-    var table = document.getElementById("todo");
-    var todoInput = document.getElementById("toDoInput");
-    setTimeout(function() {
-        filterTheTable(todoInput.value, table);
-    }, 200);
+  var table = document.getElementById("todo");
+  var todoInput = document.getElementById("toDoInput");
+  filterTheTable(todoInput.value, table);
 }
 
 //对staff表格添加过滤函数
 function appendOnkeydownForStaff() {
-    var table = document.getElementById("staff");
-    var todoInput = document.getElementById("staffInput");
-    setTimeout(function() {
-        filterTheTable(todoInput.value, table);
-    }, 200);
+  var table = document.getElementById("staff");
+  var todoInput = document.getElementById("staffInput");
+  filterTheTable(todoInput.value, table);
 }
 
 //过滤函数
 function filterTheTable(filterText, table) {
-    var tbody = table.tBodies[0];
-    var tbodyRows = tbody.rows;
-    
-    for (var i = 0; i < tbodyRows.length; i++) {
-        if (tbodyRows[i].textContent.indexOf(filterText) == -1) {
-            tbodyRows[i].style.display = "none";
-        } else {
-            tbodyRows[i].style.display = "";
+  for (var i = 0; i < table.querySelectorAll('span').length; i++) {
+    table.querySelectorAll('span')[i].className = ""
+  }
+
+  var tbody = table.tBodies[0];
+  var tbodyRows = tbody.rows;
+  
+  for (var i = 0; i < tbodyRows.length; i++) {
+    if (tbodyRows[i].textContent.toLowerCase().indexOf(filterText.toLowerCase()) == -1) {
+      tbodyRows[i].style.display = "none";
+    } else {
+      for (var j = 0; j < tbodyRows[i].cells.length; j++) {
+        if (tbodyRows[i].cells[j].textContent.toLowerCase().indexOf(filterText.toLowerCase()) != -1) {
+          var length = filterText.length;
+          var index = tbodyRows[i].cells[j].textContent.toLowerCase().indexOf(filterText.toLowerCase())
+          for (var k = index; k < index + length; k++) {
+            tbodyRows[i].cells[j].querySelectorAll('span')[k].className = 'textHighLight';
+          }
         }
+      }
+      tbodyRows[i].style.display = "";
     }
+  }
 }
